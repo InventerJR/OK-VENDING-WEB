1c0595480bc90b848588e5bb22e8ff9c78ede6ce
Braulio Alejandro Navarrete Horta <bnh74570@lasallebajio.edu.mx>
2024-10-08 19:43:03 -0600

    [FIX] Filter purchases and search purchases

--- src/app/admin/compras-almacen-fisico/page.tsx
+++ src/app/admin/compras-almacen-fisico/page.tsx
@@ -30,16 +30,14 @@ const Stock = () => {
     const [selectedCategory, setSelectedCategory] = useState('');
     const [selectedSupplier, setSelectedSupplier] = useState('');
     const [searchTerm, setSearchTerm] = useState('');
-    //add
     const { setValue, register, handleSubmit, formState: { errors } } = useForm<FormData>();
     const { loading, setLoading } = useAppContext();
     const { toastSuccess, toastError } = useToast();
     const [productList, setProductList] = useState(products);
 
-     //add
-     useEffect(() => {
+    useEffect(() => {
         fetchSuppliers();
-    }, []);
+    }, [fetchSuppliers]);
 
     useEffect(() => {
         updatelocalStorageWrapper(productList);
@@ -60,7 +58,6 @@ const Stock = () => {
         setFilters(searchTerm, selectedCategory, selectedSupplier);
     }, [searchTerm, selectedCategory, selectedSupplier, setFilters]);
 
-    //add
     const handleSupplierChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
         const selectedSupplier = suppliers.find(supplier => supplier.id === parseInt(event.target.value, 10));
         if (selectedSupplier) {
@@ -88,16 +85,12 @@ const Stock = () => {
         try {
             await registerPurchase(formData);
             toastSuccess({ message: "Se registro la compra" });
-            //closeCart();
-            //onClose();
             localStorageWrapper.removeItem('productList');
             localStorageWrapper.removeItem('registeredProducts');
-            //openTicketCart(); // Abre el modal de tickets
-            //window.location.reload(); // Usar window.location.reload() en lugar de router.reload()
         } catch (error: any) {
             toastError({ message: error.message });
             console.error("Error registering purchase:", error);
-        }finally{
+        } finally {
             setLoading(false);
         }
     };
@@ -109,63 +102,16 @@ const Stock = () => {
                     <div className='border-b-[3px] border-b-[#2C3375] w-fit px-12 self-center'>
                         <h1 className='uppercase font-bold text-3xl'>COMPRA DE PRODUCTOS</h1>
                     </div>
-                    <div>
-                        <h2 className='font-bold text-xl'>Lista de productos</h2>
-                    </div>
-                    <div className='flex flex-row gap-3 items-center'>
-                        <div className='flex flex-row gap-3 flex-wrap'>
-                            <label className='flex flex-col md:w-[240px]'>
-                                <span className='font-semibold'>Búsqueda de producto</span>
-                                <input
-                                    type='text'
-                                    className='border border-gray-300 rounded-md h-[30px] p-1'
-                                    value={searchTerm}
-                                    onChange={(e) => setSearchTerm(e.target.value)}
-                                />
-                            </label>
-                            {/*<div className='flex flex-col items-start'>
-                                <label className='flex flex-col min-w-[140px] md:w-[240px]'>
-                                    <span className='font-semibold'>Selecciona un proveedor</span>
-                                    <select
-                                        className='border border-gray-300 rounded-md h-[30px]'
-                                        {...register("supplier", { required: true })}
-                                        onChange={handleSupplierChange}
-                                    >
-                                        <option value=''>Seleccionar</option>
-                                        {suppliers && suppliers.map((supplier) => (
-                                            <option key={supplier.id} value={supplier.id}>{supplier.name}</option>
-                                        ))}
-                                    </select>
-                                    {errors.supplier && <span className="text-red-500">Este campo es requerido</span>}
-                                </label>
-                            </div>*/}
-                            {/*<div className="flex flex-col items-end space-y-2">
-                                <label className="font-semibold text-lg">Ticket Image</label>
-                                <input
-                                    type="file"
-                                    className="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100"
-                                    {...register("ticket_image", { required: true })}
-                                />
-                                {errors.ticket_image && (
-                                    <span className="text-red-500 text-sm mt-1">Este campo es requerido</span>
-                                )}
-                            </div>*/}
-                            {/*<div className='hidden xl:block w-[40px] h-[40px] ml-6'>
-                                <CartButton />
-                            </div>*/}
-                        </div>
-                        <div className='visible xl:hidden w-[40px] h-[40px] ml-6'>
-                            <CartButton />
-                        </div>
-                    </div>
                     <section>
-                        <ProductGrid searchTerm={searchTerm} selectedCategory={selectedCategory} selectedSupplier={selectedSupplier} />
+                        <ProductGrid 
+                            initialSearchTerm={searchTerm} 
+                            selectedCategory={selectedCategory} 
+                            selectedSupplier={selectedSupplier} 
+                            onSearchChange={setSearchTerm}
+                            onCategoryChange={setSelectedCategory}
+                            onSupplierChange={setSelectedSupplier}
+                        />
                     </section>
-                    {/*<div className="flex flex-col gap-2 w-full items-center py-12">
-                        <button type="submit" className="bg-[#58B7A3] text-white rounded-lg py-2 px-4 w-full">
-                            Guardar
-                        </button>
-                    </div>*/}
                 </div>
             </div>
         </main>
